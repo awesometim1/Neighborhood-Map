ea0d173ad4f6ce2b197e32327c9294311d874910
Tim Lee <awesometim1@gmail.com>
2018-03-21 20:38:58 -0400

    feat: Make js sciprt to load the markers onto the map; initialize map

---
+++ js/init.js
@@ -0,0 +1,64 @@
+if(!localStorage.rArr){
+	localStorage.rArr = JSON.stringify([
+	{
+		name:"Chickfila",
+		position: {lat:34.016716,lng:-118.282515},
+		map:map,
+	},
+	{
+		name:"Wendy's",
+		position: {lat:34.098219,lng:-118.345304},
+		map:map,
+	},
+	{
+		name:"Chickfila",
+		position: {lat:34.097752,lng:-118.338227},
+		map:map,
+	},
+	{
+		name:"Wendy's",
+		position: {lat:34.095880,lng:-118.292150},
+		map:map,
+	},
+	{
+		name:"Chickfila",
+		position: {lat:34.063535,lng:-118.445158},
+		map:map,
+	},
+	{
+		name:"Wendy's",
+		position: {lat:33.971157,lng:-118.378403},
+		map:map,
+	},
+	{
+		name:"McDonald's",
+		position: {lat:34.070610,lng:-118.268148},
+		map:map,
+	},
+	{
+		name:"Burger King",
+		position: {lat:34.116447,lng:-118.184719},
+		map:map,
+	},
+	{
+		name:"McDonald's",
+		position: {lat:34.083109,lng:-118.222829},
+		map:map,
+	},
+	{
+		name:"Taco Bell",
+		position: {lat:34.062735,lng:-118.446710},
+		map:map,
+	},
+	{
+		name:"KFC",
+		position: {lat:34.030178,lng:-118.199137},
+		map:map,
+	},
+	{
+		name:"Jack In The Box",
+		position: {lat:34.114704,lng:-118.182959},
+		map:map,
+	},
+	])
+}
