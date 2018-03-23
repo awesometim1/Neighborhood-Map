1726625c25ee56de02eeb7bcc7154891559cd835
Tim Lee <awesometim1@gmail.com>
2018-03-23 13:06:44 -0400

    feat: Implement the loadmarkers method to load markers real time and add styling for list items

--- js/app.js
+++ js/app.js
@@ -12,34 +12,62 @@ var ViewModel = function(){
 
 	this.rList = ko.observableArray([]);
 
-	loadMarkers(self.rList);
+	insMarkers(self.rList);
 
 	this.filterCats = function(name){
 		var inputBox = document.getElementById("finput").value;
 		if (inputBox.length < 1){
-			loadMarkers(self.rList);
+			insMarkers(self.rList);
 		}
 		else{
 			var delMarkers = [];
-			loadMarkers(self.rList);
+			insMarkers(self.rList);
 			self.rList().forEach(function(marker){
-				console.log(marker.name());
 				if (marker.name().indexOf(inputBox) == -1){
 					delMarkers.push(marker);
 				}
 			})
 			removeMarkers(delMarkers, self.rList);
+			loadMarkers(self.rList);
 		}
 		
 	}
+
+};
+var namesList = function(list){
+		var namesList = [];
+		list().forEach(function(el){
+			namesList.push(el.name());
+		})
+		return namesList;
+	}
+var loadMarkers = function(list){
+	var bounds = new google.maps.LatLngBounds();
+	var ca = {lat: 34.052235, lng: -118.243683};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: ca
+	});
+	JSON.parse(localStorage.rArr).forEach(function(el){
+		if (namesList(list).indexOf(el.name) != -1){
+			var marker = new google.maps.Marker({
+				position: el.position,
+				map: map
+			});
+			bounds.extend(marker.getPosition());
+		}
+	});
+	// Adjust the map zoom level so that all the markers are showing
+	map.fitBounds(bounds);
 };
 
-var loadMarkers = function(list){
+var insMarkers = function(list){
 	list.removeAll();
 	allMarkers.forEach(function(marker){
 		list.push(new Marker(marker));
 	})
 };
+
 var removeMarkers = function(mList, rList){
 	mList.forEach(function(marker){
 		rList.remove(marker);
