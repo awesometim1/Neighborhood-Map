8920faebe6a809bc2a87b2d8d89d9285e983deb5
Tim Lee <awesometim1@gmail.com>
2018-03-23 20:47:50 -0400

    refactor: Change how the map is loaded and filtered completely using global variables

--- js/app.js
+++ js/app.js
@@ -1,31 +1,65 @@
 var allMarkers = [JSON.parse(localStorage.rArr)][0];
-
-var ListItem = function(marker){
+var markers = [];
+var map;
+// Initialize the Map
+function initMap(){
+	var bounds = new google.maps.LatLngBounds();
+	var ca = {lat: 34.052235, lng: -118.243683};
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: ca
+	});
+	// If localStorage has the address array, make a marker for each address and add to bounds
+	if (localStorage.rArr){
+		JSON.parse(localStorage.rArr).forEach(function(el){
+			var marker = new google.maps.Marker({
+				position: el.position,
+				map: map
+			});
+			bounds.extend(marker.getPosition());
+			var infowindow = new google.maps.InfoWindow({
+				content: el.name
+			});
+			marker.addListener('click', function(){
+				if (marker.getAnimation()) {
+					marker.setAnimation(null);
+				} 
+				else {
+					marker.setAnimation(google.maps.Animation.BOUNCE);
+				}
+				if (infowindow.getMap()) {
+					infowindow.close()
+				} 
+				else {
+					infowindow.open(map, marker);
+				}
+			});
+			markers.push(marker);
+		});
+	}
+	// Adjust the map zoom level so that all the markers are showing
+	if (!bounds.isEmpty()){
+		map.fitBounds(bounds);
+	}
+}
+var ListItem = function(marker, index){
 
 	var self = this;
 
-	this.position = ko.observable(marker.position);
 	this.name = ko.observable(marker.name);
+	this.index = index;
 
-	//Google's animation function
-			function toggleBounce() {
-				if (marker.getAnimation() !== null) {
-					marker.setAnimation(null);
-				} else {
-					marker.setAnimation(google.maps.Animation.BOUNCE);
-				}
-				infowindow.open(map, marker);
-			}
-	// this.animate = function(marker) {
-	// 	console.log(typeof(marker))
-	// 	console.log(this)
-	// 	if (marker.getAnimation() !== null) {
-	// 		marker.setAnimation(null);
-	// 	} else {
-	// 		marker.setAnimation(google.maps.Animation.BOUNCE);
-	// 	}
-	// }
+	this.toggleClick = function(){
+		var marker = markers[self.index];
+		if (marker.getAnimation()) {
+			marker.setAnimation(null);
+		} 
+		else {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+		}
+	}
 };
+
 var ViewModel = function(){
 
 	var self = this;
@@ -34,8 +68,8 @@ var ViewModel = function(){
 
 	insMarkers(self.rList);
 
-	// Function to filter cats. Makes use of external functions loadMarkers, insMarkers, and removeMarkers
-	this.filterCats = function(name){
+	// Function to filter the list. Makes use of external functions loadMarkers, insMarkers, and removeMarkers
+	this.filterList = function(name){
 		var inputBox = document.getElementById("finput").value;
 		if (inputBox.length < 1){
 			insMarkers(self.rList);
@@ -50,7 +84,7 @@ var ViewModel = function(){
 				}
 			})
 			removeMarkers(delMarkers, self.rList);
-			loadMarkers(self.rList);
+			loadMarkers(indexList(self.rList));
 		}
 		
 	}
@@ -58,39 +92,26 @@ var ViewModel = function(){
 };
 
 // Function to make a list of names from ko.observablearray
-var namesList = function(list){
-	var namesList = [];
+var indexList = function(list){
+	var indexList = [];
 	list().forEach(function(el){
-		namesList.push(el.name());
+		indexList.push(el.index);
 	})
-	return namesList;
+	return indexList;
 }
 
 // Function to load the filtered markers into the map and adjust bounds accordingly
-var loadMarkers = function(list){
+var loadMarkers = function(iList){
 	var bounds = new google.maps.LatLngBounds();
-	var ca = {lat: 34.052235, lng: -118.243683};
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 12,
-		center: ca
-	});
-	JSON.parse(localStorage.rArr).forEach(function(el){
-		if (namesList(list).indexOf(el.name) != -1){
-			var marker = new google.maps.Marker({
-				position: el.position,
-				map: map
-			});
-			var infowindow = new google.maps.InfoWindow({
-				content: el.name			
-			});
-			marker.addListener('click', function(){
-				infowindow.open(map, marker);
-			});
-
-		
-		bounds.extend(marker.getPosition());
+	for (var i =0; i < markers.length; i++){
+		if(iList.includes(i)){
+			markers[i].setVisible(true);
+			bounds.extend(markers[i].getPosition());
+		}
+		else{
+			markers[i].setVisible(false);
+		}
 	}
-});
 	// Adjust the map zoom level so that all the markers are showing
 	if (!bounds.isEmpty()){
 		map.fitBounds(bounds);
@@ -101,8 +122,10 @@ var loadMarkers = function(list){
 // Function to insert markers into the ko.observablearray
 var insMarkers = function(list){
 	list.removeAll();
+	var index = 0;
 	allMarkers.forEach(function(marker){
-		list.push(new ListItem(marker));
+		list.push(new ListItem(marker,index));
+		index++;
 	})
 };
 
